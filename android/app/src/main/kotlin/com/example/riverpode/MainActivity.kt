30df47cbcff810e88c0d42c630e3c52020c14be4
Ibrahim Omair <ibrahim.22cs173@student.uomosul.edu.iq>
2025-09-19 03:00:03 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/riverpode/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.riverpode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+final getDate = Provider((ref) => DateTime.now());
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final now = ref.watch(getDate);
+    return Scaffold(
+      appBar: AppBar(title: Text('Demo'), centerTitle: true),
+      body: Center(child: Text(now.toString())),
+    );
+  }
+}
